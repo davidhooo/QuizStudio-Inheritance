217a247e4b35daabcafb563faf7cb3dac19eb5f0
David Ho <davidfromcali@gmail.com>
2020-07-17 09:02:53 -0400

    initialized classes

---
+++ src/Question.java
@@ -0,0 +1,15 @@
+package src;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Question {
+
+    private String questionString;
+    private ArrayList<String> answer;
+
+    public void Question(String questionString, ArrayList<String> answer) {
+        this.questionString = questionString;
+        this.answer = answer;
+    }
+
+}
---
+++ src/QuizRunner.java
@@ -0,0 +1,7 @@
+package src;
+
+public class QuizRunner {
+    public static void main(String[] args) {
+
+    }
+}
