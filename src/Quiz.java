40892c2a78bd1530a8e055a4ae0aabee1095ea2c
David Ho <davidfromcali@gmail.com>
2020-07-18 17:17:44 -0400

    restructed Quiz class and added method to add questions

--- src/Quiz.java
+++ src/Quiz.java
@@ -1,6 +1,41 @@
 package src;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 
 public class Quiz {
 
-    TrueFalseQuestion question1 = new TrueFalseQuestion("Is the Earth Flat?", "False");
+    private ArrayList<Question> listOfQuestions = new ArrayList<>();
+
+    public void addTrueFalseQuestion(String questionString, String answerString) {
+        TrueFalseQuestion newQuestion = new TrueFalseQuestion(questionString, answerString);
+        listOfQuestions.add(newQuestion);
+    }
+
+    //TO DO: add addQuestions for other type of questions
+    // get input method (scanner) make input method to get answer
+
+    //getQuestion
+    //scanner method for input
+    //isCorrect method
+
+    //askQuestion method
+
+
+//    System.out.println("Question 1: ");
+//    System.out.println(question1.getQuestion());
+
+
+//    do {
+//        userInput = input.nextLine();
+//    } while (!userInput.equals(""));
+//
+//    System.out.println("You entered: " + userInput);
+//    System.out.println(quiz.question1.isCorrect(userInput));
+//
+//    System.out.println("The answer is: ");
+//    System.out.println(quiz.question1.getAnswer());
+//    System.out.println(quiz.question1.getQuestionType());
+
+
 }
--- src/QuizRunner.java
+++ src/QuizRunner.java
@@ -4,25 +4,10 @@ import java.util.Scanner;
 public class QuizRunner {
     public static void main(String[] args) {
         Quiz quiz = new Quiz();
-        Scanner input = new Scanner(System.in);
-        String userInput;
-
-//        SHITTY CASTING EXAMPLE (if question1 object was of QUESTION type)
-//        Question qqq = quiz.question1;
-//        System.out.println(((TrueFalseQuestion)qqq).getAnswer());
-
-        System.out.println("Question 1: ");
-        System.out.println(quiz.question1.getQuestion());
-
-        do {
-            userInput = input.nextLine();
-            } while (!userInput.equals(""));
-
-        System.out.println("You entered: " + userInput);
-        System.out.println(quiz.question1.isCorrect(userInput));
-
-        System.out.println("The answer is: ");
-        System.out.println(quiz.question1.getAnswer());
-        System.out.println(quiz.question1.getQuestionType());
+        quiz.addTrueFalseQuestion("is dog an animal", "true");
     }
 }
+
+//        TrueFalseQuestion question1 = new TrueFalseQuestion("TRUE/FALSE: Is the Earth Flat?", "False");
+//        TrueFalseQuestion question2 = new TrueFalseQuestion("TRUE/FALSE: Is the sun a Star", "False");
+//        TrueFalseQuestion question3 = new TrueFalseQuestion("TRUE/FALSE: Is the Earth Flat?", "False");
